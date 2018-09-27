aa31e70ee48b7cf4017e8a702eef0292d37ffed2
kevin.xie <qingchun.xie@amd.com>
2018-09-27 18:09:00 +0800

    Add layer_norm op.
    Change-Id: I81f97746e68f8b51b08ed820caa4334e105dca72

--- framework/operators/layer_norm.cpp
+++ framework/operators/layer_norm.cpp
@@ -1,3 +1,18 @@
+/* Copyright (c) 2018 Anakin Authors, Inc. All Rights Reserved.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+   
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License. 
+*/
+
 #include "framework/operators/layer_norm.h"
 
 namespace anakin{
@@ -45,6 +60,11 @@ Status LayerNormHelper<Ttype, Ptype>::InferShape(const std::vector<Tensor4dPtr<T
     return Status::OK();
 }
 
+#ifdef AMD_GPU
+INSTANCE_LAYERNORM(AMD, Precision::FP32);
+template class LayerNormHelper<AMD, Precision::FP32>;
+ANAKIN_REGISTER_OP_HELPER(LayerNorm, LayerNormHelper, AMD, Precision::FP32);
+#endif
 
 #ifdef USE_CUDA
 INSTANCE_LAYERNORM(NV, Precision::FP32);
@@ -67,6 +87,10 @@ ANAKIN_REGISTER_OP_HELPER(LayerNorm, LayerNormHelper, ARM, Precision::FP32);
 //! register op
 ANAKIN_REGISTER_OP(LayerNorm)
 .Doc("LayerNorm operator")
+
+#ifdef AMD
+.__alias__<AMD, Precision::FP32>("layernorm")
+#endif
 #ifdef USE_CUDA
 .__alias__<NV, Precision::FP32>("layernorm")
 #endif
