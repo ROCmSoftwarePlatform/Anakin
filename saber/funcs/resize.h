5535f2c87ef140266028da8ee7b4a857d5c9a497
hjack <jack.he@amd.com>
2018-10-15 20:56:44 +0800

    Add Resize Operator--build pass only
    Change-Id: Id5ef83904f4e27ff6436542a6e824c5f4169b20d

--- saber/funcs/resize.h
+++ saber/funcs/resize.h
@@ -27,8 +27,8 @@
 #include "saber/funcs/impl/x86/saber_resize.h"
 #endif
 
-#ifdef AMD_GPU 
-#include "saber/funcs/impl/impl_resize.h"
+#ifdef AMD_GPU
+#include "saber/funcs/impl/amd/include/saber_resize.h"
 #endif
 
 #ifdef USE_ARM_PLACE
--- test/saber/test_saber_resize.cpp
+++ test/saber/test_saber_resize.cpp
@@ -1,3 +1,17 @@
+/* Copyright (c) 2018 Anakin Authors, Inc. All Rights Reserved.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
 
 #include "saber/core/context.h"
 #include "saber/funcs/resize.h"
@@ -80,7 +94,33 @@ TEST(TestSaberFunc, test_func_resize){
         }
     }
 
-    
+
+#endif
+#ifdef AMD_GPU
+
+    LOG(INFO)<<"AMD test......";
+    //Init the test_base
+    Env<AMD>::env_init();
+    TestSaberBase<AMD, AMDHX86, AK_FLOAT, Resize, ResizeParam> testbase;
+    for (int num_in : {3, 5, 8}) {
+        for (int c_in : {3, 5, 8}) {
+            for (int h_in : {3, 5, 8}) {
+                for (int w_in : {2, 5, 8}) {
+                    for (float scale_w : {1.0f, 3.3f}) {
+                        for (float scale_h : {1.0f, 4.4f}) {
+                                LOG(INFO) << scale_w << "   " << scale_h;
+                                ResizeParam<AMD> param(scale_w, scale_h);
+                                testbase.set_param(param);
+                                testbase.set_input_shape(Shape({num_in, c_in, h_in, w_in}));
+                                testbase.run_test(resize_cpu<float, AMD, AMDHX86>, 0.001);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
 #endif
 
 #ifdef USE_X86_PLACE
