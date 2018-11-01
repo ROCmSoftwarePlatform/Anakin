d21d65e193dd5f82f4ab17596c8e495e44d777bf
wun-guo.huang <wun-guo.huang@amd.com>
2018-11-01 15:08:11 +0800

    empty git
    Change-Id: I0f4cdd95bcff01483542c3999aac23c9d1ca5b89

--- tools/external_converter_v2/parser/pbs/__init__.py
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env python
-# Copyright (c) 2017, Cuichaowen. All rights reserved.
-# -*- coding: utf-8 -*-
-
-try:
-    from caffe_pb2 import *
-except ImportError:
-    raise ImportError(' No module named caffe_pb2 . ')
-
