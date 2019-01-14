f69e6ba2a891ca49c7eb068b3a9aa3a3388c372e
CM Chen <chingmin.chen@amd.com>
2019-01-14 17:08:56 -0500

    [FC] workaround before fixing cl kernel problem
    Change-Id: Ibb595cf8e5de521e3348b3c266f0ade0526e62e0

--- saber/funcs/impl/amd/vender_fc.cpp
+++ saber/funcs/impl/amd/vender_fc.cpp
@@ -300,6 +300,8 @@ SaberStatus VenderFc<AMD, OpDtype>::create(
         _branch = 0;
     }
 
+    _branch = 0; //workaround
+
     if (!param.is_transpose_weights) {
         if (_branch) {
             if (batch_size_index == BATCH_SIZE_GE_INDEX) {
