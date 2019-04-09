bef024e41efd56812047c3d7af398616d93ecc46
hjack <jack.he@amd.com>
2019-04-09 21:46:16 +0800

    remove the "AMD Summary log" of Power op
    Change-Id: I5b1d580fb59cb75c293ce504ade1b8a48c15786d

--- saber/funcs/impl/amd/saber_power.cpp
+++ saber/funcs/impl/amd/saber_power.cpp
@@ -39,17 +39,6 @@ SaberStatus SaberPower<AMD, OpDtype>::create(
 
     LOG_IF_S(INFO, ENABLE_AMD_DEBUG_LOG) << "create";
 
-    LOG_IF_S(INFO, ENABLE_AMD_DEBUG_LOG)
-            << "AMD Summary: input size N " << inputs[0]->num()
-            << " C " << inputs[0]->channel()
-            << " H " << inputs[0]->height()
-            << " W " << inputs[0]->width();
-
-    LOG_IF_S(INFO, ENABLE_AMD_DEBUG_LOG)
-            << "AMD Summary: op param power " << param.power
-            << " scale " << param.scale
-            << " shift " << param.shift;
-
     const int count = outputs[0]->valid_size();
     Shape shape({inputs[0]->dims(), 1, 1, 1});
     _in_steps.re_alloc(shape);
